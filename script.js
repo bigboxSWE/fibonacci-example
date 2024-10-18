5d60ab33ff35f206d3f9fdf01aad71b0e3cf13ce
4yman <170770027+4yman-0@users.noreply.github.com>
2024-10-18 10:44:03 +0100

    Use const
    Constants are reset for each function call duh.
    ```javascript
    function meh (n) { const x = n; return x }
    ```

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ for (i = 2; i <= 10; i++) {
 // make it good
 
 function fibonacci(n) {
-   return n < 1 ? 0
-        : n <= 2 ? 1
-        : fibonacci(n - 1) + fibonacci(n - 2)
+  return n < 1 ? 0
+       : n <= 2 ? 1
+       : fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 console.log(fibonacci(4))
@@ -21,8 +21,8 @@ console.log(fibonacci(4))
 // make it fast
 
 function fibonnaciFaster(n) {
-    let phi = (1 + Math.sqrt(5))/2;
-    let asymp = Math.pow(phi, n) / Math.sqrt(5);
-    return Math.round(asymp);
+  let phi = (1 + Math.sqrt(5))/2;
+  let asymp = Math.pow(phi, n) / Math.sqrt(5);
+  return Math.round(asymp);
 }
 
